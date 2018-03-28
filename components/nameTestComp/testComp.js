182ba2c1c5c5323a5805a35f642a0abaff344c96
cjl <531502166@qq.com>
2018-03-28 18:14:15 +0800

    更新

--- components/nameTestComp/testComp.js
+++ components/nameTestComp/testComp.js
@@ -5,7 +5,27 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    customStyle: {
+    testBg: {
+      type: String,
+      value: 'background:url(../../images/test_bg_1.png)'
+    },
+    customInputStyle: {
+      type: String,
+      value: ''
+    },
+    title: {
+      type: String,
+      value: '未来履历'
+    },
+    sampleGraph: {
+      type: String,
+      value: '../../images/example.png'
+    },
+    inputTips: {
+      type: String,
+      value: '输入姓名作为测算结果的使用名称'
+    },
+    btnStyle: {
       type: String,
       value: ''
     }
@@ -15,9 +35,19 @@ Component({
    * 组件的初始数据
    */
   data: {
-    
+    testBg: "",
+    customInputStyle: "",
+    title: "",
+    sampleGraph: "",
+    inputTips: "",
+    btnStyle: ""
   },
-
+  attached: function () {
+    for (let x in this.properties) {
+      this.properties.x && (this.data.x = this.properties.x);
+      this.setData(this.data);
+    };
+  },
   /**
    * 组件的方法列表
    */
